4f77c4f1b6a9f933a8fe6a3020a869f0eb91a93d
unknown <ngangamichael83@gmail.com>
2021-10-28 09:07:49 +0300

    added mongo db realm and went serverless

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/api/v1/restaurants',
+    baseURL: 'https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-mcbdt/service/restuarants/incoming_webhook/',
     headers: {'Content-type': 'application/json'}
 });
